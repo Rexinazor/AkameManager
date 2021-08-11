d33f834401af88faa6a7ac2fc443b1c109ceda90
Ｒｅｘｉｎａｚｏｒ <85327329+Rexinazor@users.noreply.github.com>
2021-08-11 15:56:35 +0530

    Update gtranslator.py

--- Manager/modules/gtranslator.py
+++ Manager/modules/gtranslator.py
@@ -109,8 +109,7 @@ def totranslate(bot: Bot, update: Update):
         update.effective_message.reply_text(
             "Reply to messages or write messages from other languages ​​for translating into the intended language\n\n"
             "Example: `/tr en ml` to translate from English to Malayalam\n"
-            "Or use: `/tr ml` for automatic detection and translating it into Malayalam.\n"
-            "See [List of Language Codes](t.me/OnePunchSupport/12823) for a list of language codes.",
+            "Or use: `/tr ml` for automatic detection and translating it into Malayalam.\n",
             parse_mode="markdown", disable_web_page_preview=True)
     except ValueError:
         update.effective_message.reply_text("The intended language is not found!")
